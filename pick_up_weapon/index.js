43893ce509d5a9f791b399c755396248d3bef124
Đặng Quang Anh <42268330+Anh1223@users.noreply.github.com>
2018-10-31 17:37:06 +0700

    Add files via upload

--- pick_up_weapon/index.js
+++ pick_up_weapon/index.js
@@ -1,7 +1,10 @@
 var x = screen.availWidth - 20;
-var x_cua0_dau = x/1.97
-var x_cua0_cuoi = x/1.58
-var toadocua0 = [x_cua0_cuoi,568,x_cua0_dau,110]
+var y = x/2.39;
+var x_cua0_1 = x/1.97
+var y_cua0_1 = y/5.02
+var x_cua0_2 = x/1.59
+var y_cua0_2 = y
+var toadocua0 = [x_cua0_1,y_cua0_1,x_cua0_2,y_cua0_2]
 
 document.getElementById("gg").width = x;
 document.getElementById("toadocua0").coords = toadocua0
